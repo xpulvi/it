e6abbdd95e05400cf187d08933174b74c08ddacf
xpulvi <pulvirenti.n.a@gmail.com>
2022-04-11 17:54:07 +0200

    VariablesMethodsConstructor_02

---
+++ VariablesMethodsConstructor_02/Student.java
@@ -0,0 +1,16 @@
+package it.VariablesMethodsConstructor_02;
+
+public class Student {
+
+    public String name;
+    public int grade;
+
+    public void studentName (String name, int grade){
+
+    }
+
+    public String getStudentDetails(){
+       return name + grade;
+    }
+
+}
---
+++ VariablesMethodsConstructor_02/Teacher.java
@@ -0,0 +1,15 @@
+package it.VariablesMethodsConstructor_02;
+
+public class Teacher {
+
+    String teacherName;
+    Teacher teacher = new Teacher();
+
+
+    public void assignGrade(Student alum, int finalGrade){
+
+    }
+
+
+
+}
