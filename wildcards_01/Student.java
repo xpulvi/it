555b0b2411efbd0ef9821b20eb8331542ed89899
xpulvi <pulvirenti.n.a@gmail.com>
2022-05-01 02:19:10 +0200

    addTesting

--- wildcards_01/Student.java
+++ wildcards_01/Student.java
@@ -1,15 +1,51 @@
 package it.wildcards_01;
 
+import java.util.List;
+
 public class Student {
 
     String studentName;
     String studentSurname;
     double marksAverage;
 
+    //metodo costrutore
     public Student(String name, String surname, double average){
-
+        this.studentName = name;
+        this.studentSurname = surname;
+        this.marksAverage = average;
     }
 
-    //public static getAverageMark();
+    public static getAverageMark(List listOfMarks){
+
+        double listOfMarks;
+        /*- calculates the average using:
+    - `listOfMarks.get(0).doubleValue()` to get the first value from the list
+    - `listOfMarks.get(1).doubleValue()` to get the second value from the list
+    - the arithmetic summation operator and the arithmetic division operator
+        * */
+        listOfMarks.get(0).doubleValue();
+
+        listOfMarks.get(1).doubleValue();
+
+        return this.marksAverage;
+    };
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName='" + studentName + '\'' +
+                ", studentSurname='" + studentSurname + '\'' +
+                ", marksAverage=" + marksAverage +
+                '}';
+    }
+
+    /*- considering that:
+    - there are two types of marks for the students:
+        - a`List` of 2 `Double` elements for **individual marks**
+        - a `List` of 2 `Integer` elements for **project marks**
+    - `Lisa Stuart` has a `List` called `individualMarksListLisa` of **individual marks**
+    - `Jeremy Green` has a `List` called `projectMarksListJeremy` of **project marks**
+    * */
+
 
 }
---
+++ wildcards_01/Testing.java
@@ -0,0 +1,22 @@
+package it.wildcards_01;
+
+import java.util.List;
+
+public class Testing {
+    public static void main(String[] args) {
+
+        List individualMarksListLisa;
+
+        //add two random double values to the list using the .add() built-in method of the List
+        individualMarksListLisa.add(double 12, double 22);
+
+        //create the projectMarksListJeremy list
+        List projectMarksListJeremy;
+        //add two random int values to the list using the .add() built-in method of the List
+        projectMarksListJeremy.add(int 10, int 5);
+
+        /*calculate (using the static method
+        Student.getAverageMark()) the marks average double values for lisaAvg and jeremyAvg */
+        Student.
+    }
+}
