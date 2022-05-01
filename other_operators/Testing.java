0016c5180cb780c5ca527579a12531c1de5865c8
xpulvi <pulvirenti.n.a@gmail.com>
2022-05-01 02:35:06 +0200

    add..

--- other_operators/Testing.java
+++ other_operators/Testing.java
@@ -8,8 +8,15 @@ public class Testing {
          the a variable (e.g. a = a + 5 is not accepted)
         * */
         a += 5;
-
-
+        int c = 4;
+        int i = a - c;
+        int b = 0;
+        // assign the value of `a`to `int b` and increment`b` 
+        // by one without using `a` or `int b = b + 1`
+        b = a;
+        System.out.println("e dispari: " + b);
+        b++;
+        System.out.println("e multiplo di 3: " + b);
 
     }
 }
