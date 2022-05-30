29fb44a1dde23008cdabaae8a424d7f87b5eb9b2
xpulvi <pulvirenti.n.a@gmail.com>
2022-05-30 16:42:36 +0200

    while_loop_02End

--- while_loop_02/Main.java
+++ while_loop_02/Main.java
@@ -1,13 +1,21 @@
 package it.while_loop_02;
 
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-        int num;
-        String stop;
 
+        String scriviQualcosa = " ";
+        Scanner scanner = new Scanner(System.in);
 
+        while (!scriviQualcosa.equals("stop")){
+
+            System.out.println("scrivi qualcosa. per stopare scrivi stop ");
+            scriviQualcosa = scanner.nextLine();
+            System.out.println(scriviQualcosa);
+        }
 
     }
 }
