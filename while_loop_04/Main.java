c20141a25f2ff897ecc8cfc704d447b343f88c61
xpulvi <pulvirenti.n.a@gmail.com>
2022-07-20 12:54:27 +0200

    while_loop_04

---
+++ while_loop_04/Main.java
@@ -0,0 +1,18 @@
+package it.while_loop_04;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int nuber;
+        int upperbound = 25;
+        while (true){
+            System.out.println("indovina il valore di numero: ");
+
+            System.out.println("Congratulations!" );
+        }
+    }
+}
